e53e25e1a2af9c0aaff265539119614d789377d4
Romzi Fauzi <romziflubis@gmail.com>
2022-03-25 08:28:55 +0700

    small adjustment

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 const router = require('./router/ContentCreator')
 const session = require('express-session')
 const path = require("path")
